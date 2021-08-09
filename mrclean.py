96c2e73653bf631319db0ee2ec615fcda83c8659
charlie_kickflip <c.p0yne@gmail.com>
2021-08-09 16:41:40 -0700

    first commit: added .gitignore and started mrclean.py

---
+++ mrclean.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+findrug = 'data/Drug_sensitivity_(PRISM_Repurposing_Primary_Screen)_19Q4.csv'
+findep = 'data/CRISPR_gene_dependency_Chronos.csv'
+fineff = 'data/CRISPR_gene_effect.csv'
